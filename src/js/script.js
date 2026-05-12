bfef676d47411c9ff118f9ade53d9a9d7cdd751c
Mmatheusmorais <matheusmarcelinodemorais@gmail.com>
2026-05-12 11:50:49 -0300

    Feat: arrumando funcionalidades da câmera

--- src/js/script.js
+++ src/js/script.js
@@ -1,30 +1,32 @@
 // DECLARAÇÕES DOS ELEMENTOS USANDO DOM
 
-const videoElemento = document.getElementById("Video");
-const botaoScanear = document.getElementById("btn-texto");
+const videoElemento = document.getElementById("video");
+const botaoScanear= document.getElementById("btn-texto");
 const resultado = document.getElementById("resultado");
 const canvas = document.getElementById("canvas");
 
-// FUNÇÃO QUE HABILITA A CÂMERA
 
-async function configurarCamera() {
+//FUNÇÃO QUE HABILITA A CÂMERA
+
+async function configurarCamera(){
     try{
         const midia = await navigator.mediaDevices.getUserMedia({
-            video: {facingMode: "environment"},// aciona a câmera traseira
-            audio: false
-        })
+            video: { facingMode: "environment" },// aciona a camera traseira
+            audio:false
+        });
+        // recebe a função midi para habilitar a camera
         videoElemento.srcObject = midia;
-        //garante que o vídeo comece,
+        //garante que o video comece
         videoElemento.play();
 
     }catch(erro){
         resultado.innerText="Erro ao acessar a camera",erro
     }
 }
-// executa a função da câmera 
+//executa a função da câmera
 configurarCamera();
 
-// função para ler o texto que a câmera pegar
+// Função para ler o texto que câmera pegar
 
 botaoScanear.onclick = async ()=>{
     botaoScanear.disable=true; // habilita a câmera
@@ -33,25 +35,27 @@ botaoScanear.onclick = async ()=>{
     //preparando o canvas para criar estrutura da câmera
     const contexto = canvas.getContext("2d");
 
-    // ajustar o tamanho do canvas
+    //ajustar o tamanho do canvas
     canvas.width = videoElemento.videoWidth; //largura
     canvas.height = videoElemento.videoHeight; //altura
 
-    // reset para garantir que a foto não saia invertida
-    contexto.setTransform(1, 0, 0, 1, 0, 0)
+    //reset para garantir que a foto não saia invertida
+    contexto.setTransform(1, 0, 0, 1, 0, 0);
 
-    // filtro de contraste e escala de cinza antes de tirar a foto
-    // ajuda a evitar as letras aleatórias
+    //filtro de contraste e escala de cinza antes de tirar a foto
+    //ajuda a evitar as letras aleatorias
 
-    contexto.filter = 'contrast(1.2) graysclae(1)';
+    contexto.filter = 'contrast(1.2) grayscale(1)';
     try{
+        //consumindo api
         const { data: { text }} =await Tesseract.recognize(
-            canvas, // aonde o texto vai aparecer
-            'por' // idioma do texto
+            canvas, //aonde o texto vai aparecer
+            'por' //idioma do texto
         );
-        // Remove espaços excessivos e caracteres especiais
+        // Remove espaços excessivos e caracteres especiais 
         const textoFinal = text.trim();
         resultado.innerText = textoFinal.length > 0 ? textoFinal : "Não foi possivel identificar o texto";
+
     }catch(erro){
         console.error(erro);
         resultado.innerText="Erro ao processar",erro
@@ -60,4 +64,6 @@ botaoScanear.onclick = async ()=>{
         // desabilita a câmera para fazer uma nova captura
         botaoScanear.disable=false; 
     }
+
+
 }
